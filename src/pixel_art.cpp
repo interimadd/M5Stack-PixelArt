04da0e78022bf9c05b8adba8075b3e5013565d91
interimadd <interimadd@gmail.com>
2021-02-24 22:13:34 +0900

    feat: add pixel art with frames class

--- src/pixel_art.cpp
+++ src/pixel_art.cpp
@@ -1,5 +1,20 @@
 #include "pixel_art.h"
 
+MonochromePixelArtWithFrame::MonochromePixelArtWithFrame(std::vector<MonochromePixelArt> pixel_frame_lst){
+    frame_pixel_arts_ = pixel_frame_lst;
+    frame_length_ = pixel_frame_lst.size();
+    current_frame_no_ = 0;
+}
+
+MonochromePixelArt MonochromePixelArtWithFrame::getCurrentFramePixelArt(){
+    return frame_pixel_arts_[current_frame_no_];
+}
+
+MonochromePixelArt MonochromePixelArtWithFrame::toNextFrame(){
+    if(++current_frame_no_ >= frame_length_) current_frame_no_ = 0;
+    return frame_pixel_arts_[current_frame_no_];
+}
+
 
 PixelArtManager::PixelArtManager(LCDController* lcd)
 {
--- src/pixel_art.h
+++ src/pixel_art.h
@@ -3,6 +3,17 @@
 
 using MonochromePixelArt = std::vector<std::vector<uint8_t>>;
 
+class MonochromePixelArtWithFrame{
+    private:
+        std::vector<MonochromePixelArt> frame_pixel_arts_;
+        int frame_length_;
+        int current_frame_no_;
+    public:
+        MonochromePixelArtWithFrame(std::vector<MonochromePixelArt>);
+        MonochromePixelArt getCurrentFramePixelArt();
+        MonochromePixelArt toNextFrame();
+};
+
 class PixelArtManager
 {
     private:
